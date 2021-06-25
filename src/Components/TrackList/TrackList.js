078f6fc9a952386bb0ff24ffd2b0d6c6e63d906c
sstacey <shaun.stacey1@gmail.com>
2021-06-25 14:10:29 -0500

    Refactor tracklist map function

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -7,8 +7,17 @@ import Track from '../Track/Track.js'
 class TrackList extends React.Component {
 
     render() {
-        const list = this.props.trackList.map((track) => <Track key={track.id} track={track} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} />)
-
+        // const list = this.props.trackList.map((track) => <Track key={track.id} track={track} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} />)
+        const list = this.props.trackList.map((track) => {
+            return (
+                <Track 
+                    key={track.id} 
+                    track={track} 
+                    onAdd={this.props.onAdd} 
+                    onRemove={this.props.onRemove} 
+                    isRemoval={this.props.isRemoval} />
+            ) 
+        })
         return (
             <div className="TrackList">
                 {list}
