84d46a98b7b70528daeb95f611fac2f70f666c52
sstacey <shaun.stacey1@gmail.com>
2021-06-27 06:39:53 -0500

    Fix Spotify get auth token

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,32 +1,32 @@
+let accessToken
 const clientID = 'c8005f325c344b6f8599d36639d3045c'
 const redirectURI = 'http://localhost:3000/'
-
-//test token string:  http://localhost:3000/?access_token=token123&expires_in=999999
 
 const Spotify = {
     accessToken: '',
     getAccessToken () {
-        console.log(this.accessToken)  //this is logging undefined.  Setup wrong but works.
-        if (this.accessToken) {
-            return this.accessToken
+        if (accessToken) {
+            return accessToken
         }
 
         const token = window.location.href.match(/access_token=([^&]*)/)
-        const expiresIn = window.location.href.match(/expires_in=([^&]*)/)
-        if (token && expiresIn) {
-            this.accessToken = token[1]
-            window.setTimeout(() => this.accessToken = '', expiresIn * 1000);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/)
+        if (token && expiresInMatch) {
+            accessToken = token[1]
+            const expiresIn = Number(expiresInMatch[1])
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
             window.history.pushState('Access Token', null, '/');
-            return this.accessToken
+            return accessToken
         } else {
             const url = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&redirect_uri=${redirectURI}`
             window.location = url
         }
     },
     async search(term) {
+        const accessToken = Spotify.getAccessToken()
         const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
             headers: {
-                Authorization: 'Bearer ' + this.getAccessToken()
+                Authorization: 'Bearer ' + accessToken
             }
         })
         const jsonResponse = await response.json()
